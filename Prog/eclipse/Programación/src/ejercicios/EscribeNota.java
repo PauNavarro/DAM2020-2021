82c2a2255c391f19183305eef17034f19ecf9223
PauNavarro <paunavarropalao123@gmail.com>
2020-10-16 14:01:28 +0200

    Ejercicios de programación

---
+++ Prog/eclipse/Programación/src/ejercicios/EscribeNota.java
@@ -0,0 +1,66 @@
+package ejercicios;
+
+import java.util.Scanner;
+
+public class EscribeNota {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		Scanner teclado = new Scanner(System.in);
+
+		// Escribe un programa que lea una calificación numérica entre 0 y 10 y la
+		// transforma en calificación alfabética, escribiendo el resultado.
+		
+		int nota;
+		String notaTexto;
+		
+		System.out.println("Dime la calificación numérica");
+		nota = teclado.nextInt();
+		
+		switch(nota) {
+		
+		case 1 : notaTexto = "Muy deficiente";
+		System.out.println("La nota es " + notaTexto);
+		break;
+		
+		case 2 : notaTexto = "Muy deficiente";
+		System.out.println("La nota es " + notaTexto);
+		break;
+		
+		case 3 : notaTexto = "Insuficiente";
+		System.out.println("La nota es " + notaTexto);
+		break;
+		
+		case 4 : notaTexto = "Insuficiente";
+		System.out.println("La nota es " + notaTexto);
+		break;
+		
+		case 5 : notaTexto = "Bien";
+		System.out.println("La nota es " + notaTexto);
+		break;
+		
+		case 6 : notaTexto = "Bien";
+		System.out.println("La nota es " + notaTexto);
+		break;
+		
+		case 7 : notaTexto = "Notable";
+		System.out.println("La nota es " + notaTexto);
+		break;
+		
+		case 8 : notaTexto = "Notable";
+		System.out.println("La nota es " + notaTexto);
+		break;
+		
+		case 9 : notaTexto = "Sobresaliente";
+		System.out.println("La nota es " + notaTexto);
+		break;
+		
+		case 10 : notaTexto = "Sobresaliente";
+		System.out.println("La nota es " + notaTexto);
+		break;
+		}
+		teclado.close();
+	}
+
+}
