6068a623aa9d558b20188e9e13f9bff8ea62e40b
PauNavarro <paunavarropalao123@gmail.com>
2020-10-19 10:59:23 +0200

    ejercicios de programación

--- Prog/eclipse/Programación/src/ejercicios/MuestraMayor.java
+++ Prog/eclipse/Programación/src/ejercicios/MuestraMayor.java
@@ -20,14 +20,19 @@ public class MuestraMayor {
 		System.out.println("Dame el segundo numero");
 		numero2 = teclado.nextDouble();
 
-		if (numero1 > numero2) {
-			System.out.println(numero1 + " > " + numero2);
-			System.out.println("El primer numero ( " + numero1 + " ) es mayor");
+		if (numero1 == numero2) {
+			System.out.println(numero1 + " = " + numero2);
+			System.out.println("Los numeros son equivalentes");
 		} else {
-			System.out.println(numero1 + " < " + numero2);
-			System.out.println("El segundo numero ( " + numero2 + " ) es mayor");
-		}
 
+			if (numero1 > numero2) {
+				System.out.println(numero1 + " > " + numero2);
+				System.out.println("El primer numero ( " + numero1 + " ) es mayor");
+			} else {
+				System.out.println(numero1 + " < " + numero2);
+				System.out.println("El segundo numero ( " + numero2 + " ) es mayor");
+			}
+		}
 		teclado.close();
 	}
 
