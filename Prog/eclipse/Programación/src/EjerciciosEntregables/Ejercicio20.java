8c5610f84f5123414d5d23c56e68afb1e837db24
PauNavarro <paunavarropalao123@gmail.com>
2020-11-11 08:31:25 +0100

    Copy-paste to fix previous copy-paste

--- Prog/eclipse/Programación/src/EjerciciosEntregables/Ejercicio20.java
+++ Prog/eclipse/Programación/src/EjerciciosEntregables/Ejercicio20.java
@@ -18,7 +18,7 @@ public class Ejercicio20 {
 	public static void main(String[] args) {
 
 		// Declaro e inicializo las variables
-		
+
 		Scanner teclado = new Scanner(System.in);
 
 		int cont = 1;
@@ -26,23 +26,29 @@ public class Ejercicio20 {
 		int imprimir = 0;
 
 		// Peticion de un valor al usuario para saber cual es la altura del triangulo
-		
+
 		System.out.println("Dame la altura del triangulo a imprimir");
 		numIntr = teclado.nextInt();
-		
+
+		// El bucle se ejecutara hasta que el contador sea igual al numero introducido.
+
 		for (; cont <= numIntr; cont++) {
 
-			imprimir = cont;
+			imprimir = cont; // hacemos que la variable imprimir sea igual al contador para asi imprimir el
+			// numero correcto de * cada vez
 
-			System.out.println();
-			
-			do {
+			System.out.println();	// Utilizo un println para separar cada linea de asteriscos
 
-				System.out.print("*");
+			do { 		// este bucle se encarga de imprimir los asteriscos dentro de una misma linea,
+					   // por eso utilizo un print y no un println
 
-				imprimir--;
+				System.out.print("*"); // se imprime un asterisco en la linea actual
+
+				imprimir--;			// se resta 1 a imprimir cada vez que se ejecuta el bucle hasta que imprimir = 0.
 
 			} while (imprimir > 0);
+
+			teclado.close();
 		}
 
 	}
